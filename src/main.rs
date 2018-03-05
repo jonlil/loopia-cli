c00e86b17322aeb086e7c0862e65a64228b0af22
Jonas Liljestrand <jonas.liljestrand@gmail.com>
2018-03-05 21:12:12 +0100

    parse cl arguments and query loopia

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
 #[macro_use]
 extern crate dotenv_codegen;
 
@@ -14,9 +17,30 @@ fn main() {
         String::from(dotenv!("LOOPIA_PASSWORD"))
     );
 
-    client.get_zone_records(&GetZoneRecordsRequest {
-        domain: "example.com",
-        subdomain: "@",
-        customer_number: None
-    });
+    let matches = App::new("Loopia CLI")
+        .about("Loopia CLI that wraps XMLRPC")
+        .subcommand(SubCommand::with_name("domain")
+            .subcommand(SubCommand::with_name("get-zone-records")
+                .arg(Arg::with_name("domain")
+                     .index(1))
+                .arg(Arg::with_name("subdomain")
+                     .index(2)))
+            ).get_matches();
+
+    match matches.subcommand() {
+        ("domain", Some(domain_matches)) => {
+            match domain_matches.subcommand() {
+                ("get-zone-records", Some(command)) => {
+                    client.get_zone_records(&GetZoneRecordsRequest {
+                        domain: command.value_of("domain").unwrap(),
+                        subdomain: command.value_of("subdomain").unwrap_or("@"),
+                        customer_number: None
+                    });
+                },
+                _ => unreachable!()
+            }
+        },
+        ("", None) => {},
+        _ => unreachable!(),
+    }
 }
